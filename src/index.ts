83bffa992b4cf9e2b2a879acb7dbee8984d4f9b7
Jason Reed <jcreed@gmail.com>
2019-04-20 11:28:13 -0400

    Can compute center of mass.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 type Canvas = { c: HTMLCanvasElement, d: CanvasRenderingContext2D };
 type Point = { x: number, y: number };
-type Ndict<T> = { [id: number]: T };
+type Dict<T> = { [id: string]: T };
 
 const WIDTH = 16;
 
@@ -16,7 +16,8 @@ const blue: Color = { r: 0, g: 0, b: 255 };
 
 type ConjoinedData = {
   array: SizedArray,
-  marks: Ndict<MarkType>,
+  marks: Dict<MarkType>,
+  avg: Dict<Point>,
 }
 
 function relpos(e: MouseEvent, n: Element): Point {
@@ -84,7 +85,19 @@ function colorEq(c: Color, d: Color): boolean {
 
 function findConjoined(id: ImageData): ConjoinedData {
   let counter = 1;
-  const marks: Ndict<MarkType> = {};
+  const marks: Dict<MarkType> = {};
+  const avg: Dict<Point> = {};
+  const total: Dict<number> = {};
+
+  function accumulateAvg(p: Point, mark: number) {
+    if (!avg[mark]) {
+      avg[mark] = { x: 0, y: 0 };
+      total[mark] = 0;
+    }
+    avg[mark].x += p.x;
+    avg[mark].y += p.y;
+    total[mark]++;
+  }
 
   function startFill(x: number, y: number, color: Color) {
     const mark = counter++;
@@ -97,6 +110,7 @@ function findConjoined(id: ImageData): ConjoinedData {
       if (valueGet(array, x, y)) continue;
       if (!colorEq(color, colorGet(id, x, y))) continue;
       valuePut(array, x, y, mark);
+      accumulateAvg({ x, y }, mark);
       stack.push({ x: x + 1, y });
       stack.push({ x: x - 1, y });
       stack.push({ x, y: y + 1 });
@@ -116,7 +130,12 @@ function findConjoined(id: ImageData): ConjoinedData {
       }
     }
   }
-  return { array, marks };
+
+  for (let i = 1; i < counter; i++) {
+    avg[i].x /= total[i];
+    avg[i].y /= total[i];
+  }
+  return { array, marks, avg };
 }
 
 function renderConjoined(sa: SizedArray): ImageData {
@@ -142,6 +161,11 @@ function go() {
     const dat = c1.d.getImageData(0, 0, c1.c.width, c1.c.height);
     const conj = findConjoined(dat);
     c2.d.putImageData(renderConjoined(conj.array), 0, 0);
+    Object.keys(conj.avg).forEach(i => {
+      const p = conj.avg[i];
+      c2.d.fillStyle = "yellow";
+      c2.d.fillRect(p.x - 1, p.y - 1, 2, 2);
+    });
     console.log(conj.marks);
   });
 
